7309d9e7c9637c964a4ba86ed5c67c12f7e837ff
marouane bounou <maroinbounou.123@gmail.com>
2025-07-27 18:48:48 +0100

    yarbi tkhdm

--- server/index.js
+++ server/index.js
@@ -1,28 +1,36 @@
 const express = require('express');
 const cors = require('cors');
 const invitation = require('./routes/invitation');
-
 const dotenv = require('dotenv');
 dotenv.config();
 const connectDB = require('./config/dbConnection');
 
-
 const app = express();
-
 app.use(express.json());
+
 connectDB();
+
 const corsOptions = {
-  origin: ["http://localhost:5173"],   
+  origin: process.env.NODE_ENV === 'production' 
+    ? [process.env.FRONTEND_URL] 
+    : ["http://localhost:5173"],  
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization"],
 };
+
 app.use(cors(corsOptions));
-app.use('/api',invitation)
+app.use('/api', invitation);
+
 app.get("/", (req, res) => {
   res.send("hello world!");
 });
-app.listen(5000, () => {
-    console.log('Server is running on port 5000')
-});
+
+const PORT = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
 
 module.exports = app;
