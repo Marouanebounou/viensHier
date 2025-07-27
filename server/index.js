0ac8022e73236c2d13681ab5aa6d72d1b1454f67
marouane bounou <maroinbounou.123@gmail.com>
2025-07-27 21:36:08 +0100

    index.js

--- server/index.js
+++ server/index.js
@@ -25,12 +25,11 @@ app.get("/", (req, res) => {
   res.send("hello world!");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
-if (process.env.NODE_ENV !== 'production') {
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
-}
+// Remove the condition - always start the server
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 module.exports = app;
