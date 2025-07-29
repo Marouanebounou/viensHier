7c600666af39d326ed848b668f59149fc63991c4
marouane bounou <maroinbounou.123@gmail.com>
2025-07-29 03:29:23 +0100

    commit

--- Spectacle alizawa/src/api/axios.jsx
+++ Spectacle alizawa/src/api/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://viens-hier-api.onrender.com',
+    baseURL: 'srv931649.hstgr.cloud',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
