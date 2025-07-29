e0897a30114c5e0fedf0c34b3fb326d298f48c50
marouane bounou <maroinbounou.123@gmail.com>
2025-07-29 13:02:31 +0100

    axios update

--- Spectacle alizawa/src/api/axios.jsx
+++ Spectacle alizawa/src/api/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'srv931649.hstgr.cloud:3000',
+    baseURL: 'http://srv931649.hstgr.cloud:3000/api',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
